0b64dcbdfd1da2dad174bcb82489a66a75d832cd
Jeremy Shantz <jeremy@shantz.io>
2014-12-16 00:56:19 -0500

    Polished plot1

--- plot1.R
+++ plot1.R
@@ -20,14 +20,18 @@ summarized.data <-
     group_by(year) %>% 
     summarise_each(funs(sum))
     
+summarized.data <- mutate(summarized.data, Emissions = Emissions / 1000 )
+
 # Make the basic plot
 png(filename="./plot1.png", width=480, height=480)
-plot(x = summarized.data, type='l', ylab='Emissions (tons)', xlab='Year')
+plot(x = summarized.data, type='l', ylab='Emissions (in 1000s of tons)', xlab='Year', xaxt = "n", yaxt = "n")
 
 # Annotate the plot
+axis(side=1, at=c(1999, 2002, 2005, 2008), lwd=0.5, lty=1 )
+axis(side=2, at=c(0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000), lwd=0.5, lty=1 )
 title(main=bquote(atop('Fine particulate matter ('~PM[2.5]~') emissions', 'decreased in the United States, 1999-2008' )))
-# par("col.sub" = "black")
-# par(mar =  c(5,4,4,2)) 
-# title(sub=bquote(atop('Data source: Environmental Protection Agency (EPA)')))
+par(mar =  c(2, 4, 4, 2)) 
+par(oma =  c(2, 0, 0, 0)) 
+title(sub=bquote(atop('Data source: Environmental Protection Agency (EPA)')))
 
 dev.off()
