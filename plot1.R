3bed458bf66d1b0172c9297cb609d3e4f38933b3
Jeremy Shantz <jeremy@shantz.io>
2014-12-16 00:03:03 -0500

    plot1

--- plot1.R
+++ plot1.R
@@ -1,15 +1,33 @@
+
 # National Emissions Inventory, 1999, 2002, 2005, and 2008
 
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
     # Using the base plotting system, make a plot showing the total PM2.5 emission 
     # from all sources for each of the years 1999, 2002, 2005, and 2008.
 
-# Load the data sets; check first if they are already loaded to avoid expensive reloads
+# Using dplyr to manipulate the data set
+library('dplyr')
+
+# Load the data set; check first if it is already loaded to avoid expensive reloads
 if (!exists('NEI')) {
     NEI <- readRDS("summarySCC_PM25.rds")    
 }
 
-if (!exists('SCC')) {
-    SCC <- readRDS("Source_Classification_Code.rds")    
-}
+# Select only emissions and year, group by year, then sum emissions
+summarized.data <- 
+    NEI %>% 
+    select(Emissions, year) %>% 
+    group_by(year) %>% 
+    summarise_each(funs(sum))
+    
+# Make the basic plot
+png(filename="./plot1.png", width=480, height=480)
+plot(x = summarized.data, type='l', ylab='Emissions (tons)', xlab='Year')
 
+# Annotate the plot
+title(main=bquote(atop('Fine particulate matter ('~PM[2.5]~') emissions', 'decreased in the United States, 1999-2008' )))
+# par("col.sub" = "black")
+# par(mar =  c(5,4,4,2)) 
+# title(sub=bquote(atop('Data source: Environmental Protection Agency (EPA)')))
+
+dev.off()
--- plot2.R
+++ plot2.R
@@ -1,4 +1,17 @@
+
 # National Emissions Inventory, 1999, 2002, 2005, and 2008
 
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
     # Use the base plotting system to make a plot answering this question.
+
+# using dplyr to manipulate the data set
+library('dplyr')
+
+# Load the data sets; check first if they are already loaded to avoid expensive reloads
+if (!exists('NEI')) {
+    NEI <- readRDS("summarySCC_PM25.rds")    
+}
+
+if (!exists('SCC')) {
+    SCC <- readRDS("Source_Classification_Code.rds")    
+}
