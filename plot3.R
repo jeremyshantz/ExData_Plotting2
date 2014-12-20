053c61042cdfa6bf067f7beb9933d9599739acae
Jeremy Shantz <jeremy@shantz.io>
2014-12-19 22:12:33 -0500

    plot3 complete

--- plot3.R
+++ plot3.R
@@ -5,55 +5,41 @@
     # Which have seen increases in emissions from 1999–2008? 
     # Use the ggplot2 plotting system to make a plot answer this question.
 
-# Interpretation: plot only the first and last data points in the time series 
+# Interpretation: plot the difference between the first and last data points in the time series 
     # to make clear the direction of emissions
 
 # using dplyr to manipulate the data set
 library('dplyr')
 library('ggplot2')
+library('reshape2')
 
 # Load the data sets; check first if they are already loaded to avoid expensive reloads
 if (!exists('NEI')) {
     NEI <- readRDS("summarySCC_PM25.rds")    
 }
 
-# Filter down to the Baltimore fips code,
-#   Select type emissions and year: group by year: then sum emissions
+# Filter down to the Baltimore fips code, and just the years 1999 and 2008
 summarized.data <- 
     NEI %>% 
     filter(fips == '24510') %>%
-    select(Emissions, type, year) %>%
-    group_by(year, type) %>%
-    summarise_each(funs(sum)) %>%
-    filter(year == 1999 | year == 2008)
-
-
-
-p <- ggplot(data=summarized.data, aes(x=year, y=Emissions, group=type, color=direction, shape=type ))  + 
-    geom_line() +
-    geom_point() +
-    theme(legend.position=c(.7, .4))  +
-    ggtitle("Baltimore Emissions 1999 - 2008\nPoint emissions up slightly. Other emission types down")
-
-xxx <- function(x) { 
-    print(class(x)) 
-}
-
-ddd <- 
-    NEI %>% 
-    filter(fips == '24510') %>%
-    select(Emissions, type, year) %>%
-    group_by(year, type) %>%
     filter(year == 1999 | year == 2008) %>%
-    summarise_each(funs(sum))
+    select(Emissions, type, year) %>%
+    group_by(year, type) %>% # group by year and type; then sum emissions
+    summarise_each(funs(sum)) %>% 
+    dcast(type ~ year) # reshape data, putting 1999 and 2008 as columns with type in the rows
 
-# percentchange.la <- ( losangeles.vehicles[,2] - lag(losangeles.vehicles[,2][[1]], 1)) / lag(losangeles.vehicles[,2][[1]], 1) * 100
-    
-diff <- data.frame(diff = c(1,4,2,-4), Type=c('a', 'b', 'c', 'd'))
+# Rename columsn to avoid numeric column names
+names(summarized.data) <- c('Type', 'Y1999', 'Y2008')
 
-p <- ggplot(data=diff, aes(x=Type, y=diff, color=Type, fill=Type, width=0.1)) + 
+# Calculate the difference in emissions between 2008 and 1999
+summarized.data$diff <- ddd$Y2008 - ddd$Y1999 
+
+# Create a bar plot of the difference in emissions
+p <- ggplot(data=summarized.data, aes(x=Type, y=diff, color=Type, fill=Type, width=0.1)) + 
     geom_bar(stat='identity') + 
-    ggtitle("Baltimore Emissions - Percent Change - 1999 to 2008\nPoint emissions up slightly. Other emission types down")
+    ggtitle("Baltimore Emissions - 1999 to 2008\nPoint emissions up slightly. Other emission types down") + 
+    xlab('Data source: Environmental Protection Agency (EPA)') + 
+    ylab('Emissions (in tons)')
 
-print(p)
-#ggsave(file='./plot3.png')
+# Save plot to disk
+ggsave(file='./plot3.png')
